ce5a6945ea593fba8bbfb347db2f7aac7c5a67b2
isaaczinrs2 <163526904+isaaczinrs2@users.noreply.github.com>
2025-04-19 11:59:45 -0300

    Introdução a Middlewares no Express.js

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-import http from 'http';
-import fs from 'fs';
-import rotas from './routes.js';
 import sqlite3 from 'sqlite3';
-import { sequelize, criaPedido, lePedidos } from './models.js';
+import express from 'express';
 
-const db = new sqlite3.Database('./tic.db', (erro) => {
-    if (erro) {
-        console.log('Erro ao conectar ao banco de dados', erro);
-    }
-    console.log('Banco de dados conectado com sucesso');
+
+import { sequelize } from './models.js';
+
+const app = express();
+
+app.use((req, res, next ) => {
+    console.log(`Digite 9 para falar com o atendente`);
+    next();
 });
 
-fs.writeFileSync('./mensagem.txt', 'Olá, TIC em Trilhas do arquivo!', 'utf-8');
-
-fs.readFile('./mensagem.txt', 'utf-8', (erro, conteudo) => {
-    if (erro) {
-        console.log('Falha na leitura do arquivo', erro);
-        return;
-    }
-    console.log(`Conteúdo: ${conteudo}`);
-    iniciaServidorHttp(conteudo);
+app.use((req, res, next ) => {
+    console.log(`Problema resolvido`);
+    res.send({
+        mensagem: 'Problema resolvido'
+    });
 });
 
-async function iniciaServidorHttp(conteudo) {
+app.use((req, res, next ) => {
+    console.log(`Segue o link para baixar o driver atualizado`);
+}); 
+
+async function inicializaApp() {
+
+    const db = new sqlite3.Database('./tic.db', (erro) => {
+
+        if (erro) {
+            console.log('Erro ao conectar ao banco de dados', erro);
+        }
+        console.log('Banco de dados conectado com sucesso');
+    });
+
     await sequelize.sync()
         .then(() => {
             console.log('Banco de dados sincronizado com sucesso!');
@@ -31,26 +40,11 @@ async function iniciaServidorHttp(conteudo) {
             console.log('Erro ao sincronizar o banco de dados', erro);
         });
 
-        await criaPedido({
-            valor_total: 100.00,
-            produtos: [
-              { id: 3, quantidade: 3, preco: 9.5 },
-              { id: 1, quantidade: 2, preco: 8.0 }
-            ]
-          });
-          
-          
-          const pedidos = await lePedidos();
-          console.log(JSON.stringify(pedidos, null, 2));
-    
-    const servidor = http.createServer((req, res) => {
-        rotas(req, res, { conteudo });
-    });
-
     const porta = 3002;
-    const host = 'localhost';
+    
+    app.listen(porta);
 
-    servidor.listen(porta, host, () => {
-        console.log(`Servidor executado em http://${host}:${porta}/`);
-    });
+  
 }
+
+inicializaApp();
